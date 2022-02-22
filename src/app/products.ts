b604e9356bc97e416f76fd5663277d2e5ccf0f0a
GH11PRASANTH <prasanthcse1620@gmail.com>
2022-02-22 13:43:30 +0530

    3 files modified

--- src/app/products.ts
+++ src/app/products.ts
@@ -8,20 +8,20 @@ export interface Product {
 export const products = [
   {
     id: 1,
-    name: 'Phone XL',
+    name: 'Product Name : Boost',
     price: 799,
     description: 'A large phone with one of the best screens'
   },
   {
     id: 2,
-    name: 'Phone Mini',
-    price: 699,
+    name: 'Product Name : Mobile Phone',
+    price: 50000,
     description: 'A great phone with one of the best cameras'
   },
   {
     id: 3,
-    name: 'Phone Standard',
-    price: 299,
+    name: 'Product Name :Laptops',
+    price: 40000,
     description: ''
   }
 ];
